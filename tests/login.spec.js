7c9f41b82b20d469074d59a51908d5ececc2823c
mkvczz <stnk.max@gmail.com>
2026-01-17 20:57:04 +0300

    Fix: remove nested git repository and add tests properly

---
+++ tests/login.spec.js
@@ -0,0 +1,22 @@
+const { test, expect } = require('@playwright/test');
+
+test.describe('Authorization on Sauce Demo website', () => {
+    test('User should login successfully', async ( { page }) => {
+        await page.goto('https://www.saucedemo.com/');
+        await page.locator('#user-name').fill('standard_user');
+        await page.locator('[placeholder="Password"]').fill('secret_sauce');
+        await page.locator('[data-test="login-button"]').click();
+        await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');  
+    });
+});
+
+
+test.describe('Authorization on Sauce Demo website', () => {
+    test('User should login unsuccessfully', async ( { page}) => {
+        await page.goto('https://www.saucedemo.com/');
+        await page.locator('#user-name').fill('locked_out_user');
+        await page.locator('[placeholder="Password"]').fill('secret_sauce');
+        await page.locator('[data-test="login-button"]').click();
+        await expect(page.getByText('Epic sadface: Sorry, this user has been locked out.')).toBeVisible();
+    });
+});
