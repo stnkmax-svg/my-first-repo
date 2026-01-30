77538a89687fde716dc33ba03121540e188bd7a8
mkvczz <stnk.max@gmail.com>
2026-01-30 11:55:25 +0300

    Improved my tests

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -8,6 +8,7 @@ test.describe('Authorization on Sauce Demo website', () => {
         await page.locator('[data-test="login-button"]').click();
         await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');  
     });
+    
     test('User should login unsuccessfully', async ( { page}) => {
         await page.goto('https://www.saucedemo.com/');
         await page.locator('#user-name').fill('locked_out_user');
